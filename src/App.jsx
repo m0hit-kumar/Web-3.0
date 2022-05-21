ade8baaf2bf2c10bed03c58b859d06fe53d79bd5
Mohit kumar <60257288+m0hit-kumar@users.noreply.github.com>
2022-05-21 11:18:14 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;
